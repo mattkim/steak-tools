1e43025025e17662602a1750fb735a9167f98fc0
Matt Kim <matthkim@gmail.com>
2018-01-30 22:18:53 -0800

    Cleaning up the transaction bot.  This commit has a working swap steak interval

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const http = require('http')
-const port = 3000
+var fs = require('fs');
+var https = require('https');
+var cors = require('cors');
+const http = require('http');
+const express = require('express');
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+const app = express();
+const port = 3000;
+const httpsPort = 3443;
 
-const server = http.createServer(requestHandler)
+// Load modules
+var modules = [];
+
+const TransactionBot = require('./transactionBot.js');
+
+modules.push(new TransactionBot());
+
+// cors
+app.use(cors());
+app.options('*', cors());
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!');
+})
+
+const server = http.createServer(app);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
 
+// // https creds
+// var privateKey  = fs.readFileSync('certificates/key.pem', 'utf8');
+// var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
+// var credentials = {key: privateKey, cert: certificate};
+// var httpsServer = https.createServer(credentials, app);
+
+// httpsServer.listen(httpsPort, (err) => {
+//   if (err) {
+//     return console.log('something bad happened https', err);
+//   }
+
+//   console.log(`https server is listening on ${port}`);
+// });
---
+++ transactionBot.js
@@ -0,0 +1,141 @@
+var _ = require('lodash');
+var popsicle = require('popsicle');
+var os = require('os');
+var ark = require('arkjs');
+
+// Globals
+const protocol = "http";
+const ip = "52.71.146.212";
+const port = "4100";
+const version = 43;
+const __headers = {
+	os: os.platform() + os.release(),
+	version: version, // TODO: config
+	port: port,
+	nethash: "482a090e26c3ced0cb389c63d1e42d4d55993d20623c7ebee8e630b0eaa243fd", // Steak hash
+	'Content-Type': 'application/json' // only needed when sending data.
+};
+
+const account1 = {
+	address: "JMHXMzP7vxAVvwEzg1vQZEbu1jy1AmLQjy",
+	publicKey: "035ae476a82a66f75fb8d6236507c9158609a06028fac51fbdcb462748ef5bdcb4",
+	passphrase: "machine mind cost piano water destroy grocery bounce airport very adult become",
+}
+const account2 = {
+	address: "Jffemxo47ehp3w85bkmvMqSYbALf2EqpPG",
+	publicKey: "",
+	passphrase: "auction this liar minor uphold wedding audit where into snake doll flip",
+}
+
+var __private = {
+	// hold the Peer list
+	// By default one peer by IP is accepted.
+	peers: {}
+};
+
+// Constructor
+function TransactionBot () {
+	var getPeers = this.getPeers;
+	var swapSteak = this.swapSteak;
+	setInterval(function(){
+		// getPeers().then(peers => console.log(peers));
+		swapSteak(account1, account2, 1000000000).then(res => console.log(res));
+		swapSteak(account2, account1, 1000000000).then(res => console.log(res));
+	}, 3000);
+}
+
+TransactionBot.prototype.swapSteak = function(sender, recipient, amount) {
+	ark.crypto.setNetworkVersion(43);
+
+	var tx = ark.transaction.createTransaction(
+		recipient.address,
+		amount,
+		null,
+		sender.passphrase,
+		null
+	);
+
+	// Add extra props to the transaction.
+	tx.senderId = sender.address;	
+	tx.label = 'Swap Steak';
+	tx.date = getDate(tx.timestamp);
+	tx.total = -1 * (tx.amount + tx.fee) ;
+	tx.humanTotal = tx.total / Math.pow(10, 8);
+	tx.confirmations = 0;
+	tx.hop = 1;
+
+	body = {transactions: [tx]};
+
+	return new Promise((resolve) => {
+		post("/peer/transactions", body, (res) => {
+			resolve(res);
+		});
+	});
+}
+
+TransactionBot.prototype.getAccount = function(address) {
+	return new Promise((resolve) => {
+		get("GET", `/api/accounts?address=${address}`, (res) => {
+			// TODO: how to handle error?
+			resolve(res);
+		});
+	})
+}
+
+// Get peers from seed peer.
+TransactionBot.prototype.getPeers = function() {
+	return new Promise((resolve) => {
+		get("GET", "/api/peers", (res) => {
+			if (res && res.body) {
+				res.body.peers.forEach(peer => {
+					__private.peers[peer.ip] = peer;
+					resolve(__private.peers);
+				});
+			}
+		});
+	});
+}
+
+function get(path, cb) {
+	// TODO: get a random peer for ip address.
+	var req = {
+		url: `${protocol}://${ip}:${port}${path}`,
+		method: "GET",
+		headers: __headers,
+		timeout: 2000,
+	};
+
+	var request = popsicle.request(req);
+	request.use(popsicle.plugins.parse(['json'], false)).then(function (res) {
+		cb(res);
+	});
+}
+
+function post(path, body, cb) {
+	// TODO: get a random peer for ip address.
+	var req = {
+		url: `${protocol}://${ip}:${port}${path}`,
+		method: "POST",
+		body: body,
+		headers: __headers,
+		timeout: 2000,
+	};
+
+	var request = popsicle.request(req);
+	request.use(popsicle.plugins.parse(['json'], false)).then(function (res) {
+		cb(res);
+	});
+}
+
+function getDate(timestamp) {
+	return new Date((
+		timestamp + parseInt(
+			new Date(
+				Date.UTC(2017, 2, 21, 13, 0, 0, 0)
+			).getTime() / 1000
+		)) * 1000
+	);
+}
+
+// Export
+module.exports = TransactionBot;
